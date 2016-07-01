43dc5a50902090a9cd1bcd199ae2fca090395e53
kanavrov <l.kanavrov@gmail.com>
2016-07-01 10:44:11 +0300

    Removing comment from Extensions class. Adding empty Utility class. Upgrading project.

--- Code/Extensions.cs
+++ Code/Extensions.cs
@@ -12,7 +12,6 @@ namespace CodedFilesTest
     {
         public static void SetInputText(this HtmlControl element, string text)
         {
-            // IE will respond to a 'change' event; see https://github.com/angular/angular.js/blob/master/src/ng/directive/input.js for detailed Angular code
             string script =
                 "angular.element(" + element.ClientSideLocator + ").val('" + text + "').triggerHandler('change')";
             Manager.Current.ActiveBrowser.Actions.InvokeScript(script);
